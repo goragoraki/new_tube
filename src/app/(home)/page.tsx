8f8e90774a0c698d39362aef0b7b214f96c2db35
goragoraki <gorakio0126@gmail.com>
2025-08-12 09:22:21 +0900

    completed basic layout

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,6 +3,7 @@
 export default function Home() {
   return (
     <div>
+      i will upload video
     </div>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,9 @@ export default function RootLayout({
       <body
         className={inter.className}
       >
-        {children}
+        <main>
+          {children}
+        </main>
       </body>
     </html>
   );
