dc427696588b8f9848cf50706e7b52722887e1fd
goragoraki <gorakio0126@gmail.com>
2025-09-09 17:33:50 +0900

    add protected route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isProtectedRoute = createRouteMatcher([
-    "/protected(.*)",
     "/studio(.*)",
+    "/subscriptions",
+    "/feed/subscriptions",
+    "/playlists(.*)",
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
